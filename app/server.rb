ba8ccec0631cf50df8be37944d1c43d7420583e3
kudojp <heyjudejudejude1968@gmail.com>
2023-05-28 09:14:59 +0900

    Start server and wait for the request

--- app/server.rb
+++ app/server.rb
@@ -9,9 +9,8 @@ class YourRedisServer
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    # server = TCPServer.new(@port)
-    # client = server.accept
+    server = TCPServer.new(@port)
+    client = server.accept
   end
 end
 
