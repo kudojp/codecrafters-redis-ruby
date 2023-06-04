b5c414a3265900ed98742cb6755342162759c087
kudojp <heyjudejudejude1968@gmail.com>
2023-06-04 19:23:08 +0900

    Refactor with YourRedisServer#handle_connection

--- app/server.rb
+++ app/server.rb
@@ -8,30 +8,33 @@ class YourRedisServer
   def start
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
+
     server = TCPServer.new(@port)
     loop do
-      new_socket = server.accept
-      Thread.new do
-        socket = new_socket
-        puts "handling socket #{socket.object_id} in thread #{Thread.current.object_id}"
+      handle_connection(server.accept)
+    end
+  end
 
-        loop do
-          begin
-            socket.recv(MAX_COMMAND_LENGTH)
-          rescue Errno::ECONNRESET
-            puts "The connection is probably terminated by the client."
-            break
-          end
-          
-          begin
-            socket.write("+PONG\r\n")
-          rescue Errno::ECONNRESET, Errno::EPIPE
-            puts "The connection is terminated by the client."
-            break
-          end
+  def handle_connection(socket)
+    Thread.new do
+      puts "handling socket #{socket.object_id} in thread #{Thread.current.object_id}"
+
+      loop do
+        begin
+          socket.recv(MAX_COMMAND_LENGTH)
+        rescue Errno::ECONNRESET
+          puts "The connection is probably terminated by the client."
+          break
         end
-        # socket.close
+        
+        begin
+          socket.write("+PONG\r\n")
+        rescue Errno::ECONNRESET, Errno::EPIPE
+          puts "The connection is terminated by the client."
+          break
+        end
       end
+      # socket.close
     end
   end
 end
